96814c3a986f0add39e59f89c2b4b012be4bd6f3
iFryTheReal <93728868+iFryTheReal@users.noreply.github.com>
2026-02-04 17:09:16 +0100

    Add health bar and health managment to mob

---
+++ mobs/mob.gd
@@ -0,0 +1,30 @@
+@icon("res://icons/icon_mob.svg")
+class_name Mob extends Area2D
+
+@export var health: int = 100 : set = set_health
+
+@onready var _health_bar: ProgressBar = %HealthBar
+
+var _tween: Tween = null
+
+func _ready() -> void:
+	_health_bar.max_value = health
+	set_health(health)
+
+func set_health(new_health: int) -> void:
+	health = maxi(0, new_health)
+	
+	if _health_bar != null:
+		if _tween != null:
+			_tween.kill()
+		_tween = create_tween()
+		_tween.tween_property(_health_bar, "value", health, 0.2)
+		
+	if health <= 0:
+		_die()
+
+func _die() -> void:
+	queue_free()
+
+func take_damage(amount: int) -> void:
+	health -= amount
--- turrets/weapons/projectiles/simple_rocket.gd
+++ turrets/weapons/projectiles/simple_rocket.gd
@@ -2,11 +2,14 @@ extends Area2D
 
 @export var max_distance: float = 1000.0
 @export var speed: float = 350.0
+@export var damage: float = 20.0
 
 var _traveled_distance: float = 0.0
 
 func _ready() -> void:
 	monitorable = false
+	
+	area_entered.connect(_on_area_entered)
 
 func _physics_process(delta: float) -> void:
 	position += transform.x * speed * delta
@@ -17,3 +20,8 @@ func _physics_process(delta: float) -> void:
 
 func explode() -> void:
 	queue_free()
+
+func _on_area_entered(area: Area2D) -> void:
+	if area is Mob:
+		area.take_damage(damage)
+		explode()
