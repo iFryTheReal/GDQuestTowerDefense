fa833bc02549efce62d8f326a751fe6d4deab60d
iFryTheReal <93728868+iFryTheReal@users.noreply.github.com>
2026-02-04 16:14:33 +0100

    Add turret attack behavior

--- turrets/simple_cannon.gd
+++ turrets/simple_cannon.gd
@@ -1,8 +1,18 @@
 class_name SimpleCannon extends Weapon
 
+@onready var _rocket_spawn_point: Marker2D = %RocketSpawnPoint
+
 func _physics_process(delta: float) -> void:
 	var mobs_in_range: Array[Area2D] = _area_2d.get_overlapping_areas()
 	if not mobs_in_range.is_empty():
 		var target: Area2D = mobs_in_range.front()
 		look_at(target.global_position)
-	$Mob.global_position = get_global_mouse_position()
+
+func _attack() -> void:
+	var mobs_in_range: Array[Area2D] = _area_2d.get_overlapping_areas()
+	if mobs_in_range.is_empty():
+		return
+	
+	var rocket: Node2D = preload("uid://ccfxqng1vbuqc").instantiate()
+	get_tree().current_scene.add_child(rocket)
+	rocket.global_transform = _rocket_spawn_point.global_transform
--- turrets/weapons/weapon.gd
+++ turrets/weapons/weapon.gd
@@ -2,13 +2,21 @@
 class_name Weapon extends Sprite2D
 
 @export var mob_detection_range: float = 400.0
+@export var attack_rate: float = 1.0
 
 var _area_2d: Area2D = _create_area_2d()
+
 @onready var _collision_shape_2d: CollisionShape2D = _create_collision_shape_2d()
+@onready var _timer: Timer = _create_timer()
 
 func _ready() -> void:
+	z_index = 10
+	
 	add_child(_area_2d)
 	_area_2d.add_child(_collision_shape_2d)
+	add_child(_timer)
+	_timer.start()
+	_timer.timeout.connect(_attack)
 
 func _create_area_2d() -> Area2D:
 	var area: Area2D = Area2D.new()
@@ -21,3 +29,11 @@ func _create_collision_shape_2d() -> CollisionShape2D:
 	collision_shape.shape = CircleShape2D.new() 
 	collision_shape.shape.radius = mob_detection_range
 	return collision_shape
+
+func _create_timer() -> Timer:
+	var timer: Timer = Timer.new()
+	timer.wait_time = 1.0 / attack_rate
+	return timer
+
+func _attack() -> void:
+	return
