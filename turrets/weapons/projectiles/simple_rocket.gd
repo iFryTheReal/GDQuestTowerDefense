9d1032f72590357cf01022061b40d6337523a3fe
iFryTheReal <93728868+iFryTheReal@users.noreply.github.com>
2026-02-04 15:50:32 +0100

    Create simple turrets projectile

---
+++ turrets/weapons/projectiles/simple_rocket.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+@export var max_distance: float = 1000.0
+@export var speed: float = 350.0
+
+var _traveled_distance: float = 0.0
+
+func _ready() -> void:
+	monitorable = false
+
+func _physics_process(delta: float) -> void:
+	position += transform.x * speed * delta
+	
+	_traveled_distance += speed * delta
+	if _traveled_distance > max_distance:
+		explode()
+
+func explode() -> void:
+	queue_free()
